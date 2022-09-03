7c44b3eea9a28b1e4a2cee5680c2f95ec18af131
mrrpablo <precijagz480@gmail.com>
2022-09-02 17:24:50 -0700

    color thing

---
+++ app.js
@@ -0,0 +1,12 @@
+const setTheme = theme => document.documentElement.className = theme;
+
+    document.getElementById('theme-select').addEventListener('change', function() {
+        setTheme(this.value);
+    });
+
+    const getTheme = () =>{
+        const theme = localStorage.getItem('theme');
+        theme && setTheme(theme);
+    }
+
+    getTheme();
